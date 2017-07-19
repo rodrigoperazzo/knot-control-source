194d969a303ea4b04ce9632b343be6ee998f2b51
Rodrigo Perazzo <rpr@cesar.org.br>
2017-07-19 10:16:53 -0300

    Add initial files for KNOT Control Service
    This patch adds initial files to implement service daemon and
    its CLI tool to control other daemons running in the gateway.

---
+++ tools/knotctl.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    printf("KNOT Control CLI tool\n");
+
+    return 0;
+}
