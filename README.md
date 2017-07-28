2a98b0053de98fb1015d7b822332098aa5bd3901
Rodrigo Perazzo <rpr@cesar.org.br>
2017-07-28 20:43:40 -0300

    Add command line set-user option parsing
    This patch adds set-user command option skeleton. 'set-user' command
    is going to call knot control daemon to save user's UUID and TOKEN.

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 KNoT Control is part of KNoT project. It is a service daemon running in KNoT gateway and is responsible for controlling the execution and configuration of all other services/daemons running in the gateway.
 
-Dependencies: none
+Dependencies:
+glib >= 2.28
+
+How to install dependencies:
+	sudo apt-get install glib2.0
 
 How to build (daemon):
 ```shell
@@ -11,5 +15,5 @@ How to build (daemon):
 
 How to build (CLI):
 ```shell
- $ gcc -o knotctl tools/knotctl.c
+ $ gcc $(pkg-config --cflags glib-2.0) -Wall -Werror -Wextra -Wno-unused-parameter -o knotctl tools/knotctl.c $(pkg-config --libs glib-2.0)
 ```
--- tools/knotctl.c
+++ tools/knotctl.c
@@ -7,10 +7,79 @@
  *
  */
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <glib.h>
 
-int main (int argc, char *argv[])
+static gboolean opt_set_user = FALSE;
+static char *opt_uuid;
+static char *opt_token;
+
+static int cmd_set_user(void)
 {
-    printf("KNOT Control CLI tool\n");
+	if (!opt_uuid) {
+		printf("User's UUID missing!\n");
+		return -EINVAL;
+	}
 
-    return 0;
+	if (!opt_token) {
+		printf("User's TOKEN missing!\n");
+		return -EINVAL;
+	}
+
+	printf("Saving user credentials: %s %s...\n", opt_uuid, opt_token);
+
+	return 0;
+}
+
+static GOptionEntry options[] = {
+	{ "uuid", 'u', 0, G_OPTION_ARG_STRING, &opt_uuid,
+						"User's UUID", "UUID" },
+	{ "token", 't', 0, G_OPTION_ARG_STRING, &opt_token,
+						"User's token", "TOKEN" },
+	{ NULL },
+};
+
+static GOptionEntry commands[] = {
+	{ "set-user", 0, 0, G_OPTION_ARG_NONE, &opt_set_user,
+	"Set user's credentials. " \
+	"Eg: ./knotctl --set-user -u value -t value", NULL},
+	{ NULL },
+};
+
+int main(int argc, char *argv[])
+{
+	GOptionContext *context;
+	GOptionGroup *opt_group;
+	GError *gerr = NULL;
+	int err;
+
+	opt_uuid = NULL;
+	opt_token = NULL;
+
+	printf("KNOT Control CLI tool\n");
+
+	context = g_option_context_new(NULL);
+	g_option_context_add_main_entries(context, commands, NULL);
+
+	opt_group = g_option_group_new("options", "Options usage",
+					"Show all set options", NULL, NULL);
+	g_option_context_add_group(context, opt_group);
+	g_option_group_add_entries(opt_group, options);
+
+	if (!g_option_context_parse(context, &argc, &argv, &gerr)) {
+		printf("Invalid arguments: %s\n", gerr->message);
+		g_error_free(gerr);
+		g_option_context_free(context);
+		exit(EXIT_FAILURE);
+	}
+
+	g_option_context_free(context);
+
+	if (opt_set_user)
+		err = cmd_set_user();
+
+	printf("Exiting\n");
+
+	return err;
 }
